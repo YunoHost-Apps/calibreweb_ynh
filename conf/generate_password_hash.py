05c15e969cd3da6a8fc5fcb018de93485745e879
Krakinou <misterl56@hotmail.com>
2019-01-14 23:31:49 +0100

    Initial commit for hook

--- conf/generate_password_hash.py
+++ conf/generate_password_hash.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 # Permet de générer le hash pour le password
+#Plus utilisé depuis la MAJ 0.92~ynh3 avec LDAP
 import sys
 path=sys.argv[2]
 sys.path.append(path)
