41b3a9a8886bbc7cfb32b65ca0dabb1b49775ea7
Alexandre Aubin <alex.aubin@mailoo.org>
2020-06-04 17:16:32 +0200

    Missing dependency to build Pillow ?

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-PKG_DEPENDENCIES="sqlite3 libldap2-dev libsasl2-dev python3-dev imagemagick python3-lxml libjpeg-dev"
+PKG_DEPENDENCIES="sqlite3 libldap2-dev libsasl2-dev python3-dev imagemagick python3-lxml libjpeg-dev zlib1g-dev"
 #PKG_DEPENDENCIES="sqlite3 python3-pip imagemagick"
 DOSSIER_MEDIA=/home/yunohost.multimedia
 
