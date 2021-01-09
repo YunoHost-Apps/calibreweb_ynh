adda84094db605a3559333ed20e4e924a2629176
Krakinou <misterl56@hotmail.com>
2021-01-09 15:00:41 +0100

    Restore _common.sh var and set some comments

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -3,6 +3,9 @@
 PKG_DEPENDENCIES="sqlite3 libldap2-dev libsasl2-dev python3-dev imagemagick python3-lxml libjpeg-dev"
 DOSSIER_MEDIA=/home/yunohost.multimedia
 
+#These var are used in init_calibre_db_settings conf file
+LOG_FILE=/var/log/$app/$app.log
+ACCESS_LOG_FILE=/var/log/$app/$app-access.log
 #=================================================
 # EXPERIMENTAL HELPERS
 # TO BE DELETED WHEN RELEASED
