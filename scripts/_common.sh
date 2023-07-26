dc3f2bb6a4d5c3c7000d0e6825c3a26e4c44aa37
Krakinou <misterl56@hotmail.com>
2023-07-26 08:10:14 +0200

    fix mach = uname -m

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -6,3 +6,19 @@ access_log_file=/var/log/$app/$app-access.log
 mach=`uname -m`
 
 
+
+
+case "$mach" in
+ "armv6l" ) mach="arm"
+			;;
+ "armv7l" ) mach="arm"
+			;;
+ "armv8l" ) mach="arm64"
+ 			;;
+ "aarch64" ) mach="arm64"
+ 			;;
+ "x86_64" ) mach="64bit"
+ 			;;
+ * ) mach="32bit" 
+ 	;;
+esac
