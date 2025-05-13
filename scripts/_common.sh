2501b8efe0c4d4ca0376f1f718a2d835befe68ef
Krakinou <misterl56@hotmail.com>
2025-05-13 23:32:01 +0200

    Dynamically set data_dir from calibre_dir

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -5,6 +5,14 @@ log_file=/var/log/$app/$app.log
 access_log_file=/var/log/$app/$app-access.log
 mach=`uname -m`
 
+#data_dir provisionning cannot be used as it is located into yunohost.multimedia directory and we don't want it to be moved around in case of changes
+#We should have used a standard yunohost.app folder with symlink to yunohost.multimedia but meeeh...
+#So, we dynamically set $data_dir based on $calibre_dir app settings instead of using the standard provisioning, which make it behaves
+# as a standard data directory in backup, restore, upgrade and remove script.
+if [ -n "$calibre_dir" ]; then
+	data_dir=$calibre_dir
+fi
+
 case "$mach" in
  "armv6l" ) mach="arm"
 			;;
